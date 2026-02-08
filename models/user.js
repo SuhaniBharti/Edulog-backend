8824694f71a1b782ab6549330ca34fb32d3a4664
SuhaniBharti <156918664+SuhaniBharti@users.noreply.github.com>
2026-02-08 18:10:23 +0530

    final

--- models/user.js
+++ models/user.js
@@ -1,14 +1,4 @@
-// const mongoose=require("mongoose");
 
-// const userSchema=new mongoose.Schema({
-//     name:String,
-//     email:{
-//         type:String,
-//         unique:true
-//     },
-//     password:String,
-// });
-// module.exports=mongoose.model("User",userSchema);
 
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
